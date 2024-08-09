7ea0eed41886d130770e102e8e3b156080348046
Willisfun <a0979569973@gmail.com>
2024-08-09 18:46:01 +0800

    trivial

---
+++ lib.js
@@ -0,0 +1,9 @@
+function echo(msg)
+{
+    console.log(msg);
+}
+function add (n1,n2)
+{
+    console.log(n1+n2)
+}
+export default {echo:echo,add:add};
---
+++ main.js
@@ -0,0 +1,5 @@
+import lib from "./lib.js";
+let name="我又大又肥"
+console.log(lib)
+lib.echo("hello");
+lib.add(3,4);
