bb5ab498247adc47860efe3b940b157a5efbcfa1
Willisfun <a0979569973@gmail.com>
2024-08-10 14:09:59 +0800

    入門完整課程

--- lib.js
+++ lib.js
@@ -1,9 +1,26 @@
-function echo(msg)
-{
-    console.log(msg);
-}
-function add (n1,n2)
+let add=function(n1,n2)
 {
     console.log(n1+n2)
-}
-export default {echo:echo,add:add};
+};
+let multiply=function(n1,n2)
+{
+console.log(n1*n2)
+};
+let math=
+{
+    add:add,multiply:multiply
+};
+export default math;
+export {add,multiply};
+
+
+/*
+//預設的輸出
+let x="Hello";
+//export default x;
+//非預設的輸出
+let data=[5,6,7]
+let obj={x:10,y:2}
+//export {data,obj}
+export{x as default,data,obj};
+*/
--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-import lib from "./lib.js";
-let name="我又大又肥"
-console.log(lib)
-lib.echo("hello");
-lib.add(3,4);
+import math from "./lib.js"
+console.log(math)
+math.add(3,4);
+math.multiply(-3,4)
+
+
+
+
+/*
+import a,{data,obj}from "./lib.js"
+//預設的輸入
+//import a from "./lib.js";
+console.log(a);
+//非預設的輸入
+//import {data,obj}from "./lib.js";
+console.log(data)
+console.log(obj)
+*/
